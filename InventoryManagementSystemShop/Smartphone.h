0a29a2bfa210bab19f1a39a97e62bb300f0320bc
leangseng2030-netizen <Leangseng2030@gmail.com>
2026-09-07 21:38:21 +0700

    Smartphone.h

---
+++ InventoryManagementSystemShop/Smartphone.h
@@ -0,0 +1,30 @@
+#ifndef SMARTPHONE_H
+#define SMARTPHONE_H
+#include "Product.h"
+#include <iostream>
+using namespace std;
+
+class Smartphone : public Product {
+
+  private:
+    string type;
+    string color;
+    int storage;
+
+  public:
+    Smartphone();
+    Smartphone(int id, string name, double price, int quantity,string type, string color, int storage);
+
+    virtual ~Smartphone();
+
+    string getType();
+    string getColor();
+    int getStorage();
+
+    void setType(string type);
+    void setColor(string color);
+    void setStorage(int storage);
+
+    void display() override;
+};
+#endif
