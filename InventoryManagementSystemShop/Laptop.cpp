eeb0199c675e22f0f1d06e9dfa73a50916968936
Sakhey Marvin <sakheymavin@gmail.com>
2026-09-11 10:37:14 +0700

    Finished Getter and Setter, and fully incorporated Display

--- InventoryManagementSystemShop/Laptop.cpp
+++ InventoryManagementSystemShop/Laptop.cpp
@@ -29,60 +29,36 @@ Laptop::Laptop(int id, string name, double price, int quantity, string brand,
 }
 
 // Destructor
-Laptop::~Laptop(){
-    cout << "Laptop Information Deleted" << endl;
-}
+Laptop::~Laptop() { cout << "Laptop Information Deleted" << endl; }
 
 // Getter
-int Laptop::getId() {
-  return id;
-}
-string Laptop::getBrand() {
-  return brand;
-}
-string Laptop::getModel(){
-    return model;
-}
-string Laptop::getProcessor(){
-    return processor;
-}
-string Laptop::get_DOM(){
-    return DOM;
-}
-string Laptop::get_Gpu(){
-    return gpu;
-}
-int Laptop::get_Ram(){
-    return ram;
-}
-int Laptop::get_Storage(){
-    return storage;
-}
+int Laptop::getId() { return id; }
+string Laptop::getBrand() { return brand; }
+string Laptop::getModel() { return model; }
+string Laptop::getProcessor() { return processor; }
+string Laptop::get_DOM() { return DOM; }
+string Laptop::get_Gpu() { return gpu; }
+int Laptop::get_Ram() { return ram; }
+int Laptop::get_Storage() { return storage; }
 
 // Setter
 
+void Laptop::setId(int id) { this->id = id; }
+void Laptop::setBrand(string brand) { this->brand = brand; }
+void Laptop::setModel(string model) { this->model = model; }
+void Laptop::setProcessor(string processor) { this->processor = processor; }
+void Laptop::set_Gpu(string gpu) { this->gpu = gpu; }
+void Laptop::set_DOM(string DOM) { this->DOM = DOM; }
+void Laptop::set_Ram(int ram) { this->ram = ram; }
+void Laptop::set_Storage(int storage) { this->storage = storage; }
 
-  void Laptop::setId(int id){
-    this->id = id;
-  }
-  void Laptop::setBrand(string brand){
-    this -> brand = brand;
-  }
-  void Laptop::setModel(string model){
-    this->model = model;
-  }
-  void Laptop::setProcessor(string processor){
-    this->processor = processor;
-  }
-  void Laptop::set_Gpu(string gpu){
-    this->gpu = gpu;
-  }
-  void Laptop::set_DOM(string DOM){
-    this->DOM = DOM;
-  }
-  void Laptop::set_Ram(int ram){
-    this-> ram = ram;
-  }
-  void Laptop::set_Storage(int storage){
-    this-> storage = storage;
-  }
+void Laptop::display() {
+  Product::display();
+  cout << "Brand: " << brand << endl;
+  cout << "Model: " << model << endl;
+  cout << "Processor: " << processor << endl;
+  cout << "Date of Manufacture: " << DOM << endl;
+  cout << "GPU: " << gpu << endl;
+  cout << "RAM: " << ram << "GB" << endl;
+  cout << "Storage: " << storage << "GB" << endl;
+}
--- InventoryManagementSystemShop/Product.cpp
+++ InventoryManagementSystemShop/Product.cpp
@@ -1,56 +1,38 @@
 #include "Product.h"
+
 #include <iostream>
 
-Product::Product(){
-    id = 0;
-    name = "";
-    price = 0.0;
-    quantity = 0;
+Product::Product() {
+  id = 0;
+  name = "";
+  price = 0.0;
+  quantity = 0;
 }
 
-Product::Product(int id, string name, double price, int quantity){
-    this->id = id;
-    this->name = name;
-    this->price = price;
-    this->quantity = quantity;
+Product::Product(int id, string name, double price, int quantity) {
+  this->id = id;
+  this->name = name;
+  this->price = price;
+  this->quantity = quantity;
 }
 
-Product::~Product(){
-    cout << "Product Destroyed" << endl;
-}
+Product::~Product() { cout << "Product Destroyed" << endl; }
 
 // Getter
-int Product::getId(){
-    return id;
-}
-string Product::getName(){
-    return name;
-}
-double Product::getPrice(){
-    return price;
-}
-int Product::getQuantity(){
-    return quantity;
-}
+int Product::getId() { return id; }
+string Product::getName() { return name; }
+double Product::getPrice() { return price; }
+int Product::getQuantity() { return quantity; }
 
 // Setter
-void Product::setId(int id){
-    this->id = id;
-}
-void Product::setName(string name){
-    this->name = name;
-}
-void Product::setPrice(double price){
-    this->price = price;
-}
-void Product::setQuantity(int quantity){
-    this->quantity = quantity;
-}
+void Product::setId(int id) { this->id = id; }
+void Product::setName(string name) { this->name = name; }
+void Product::setPrice(double price) { this->price = price; }
+void Product::setQuantity(int quantity) { this->quantity = quantity; }
 
-void Product::display(){
-    cout << "ID: " << id << endl;
-    cout << "Name: " << name << endl;
-    cout << "Price: "<< price << endl;
-    cout << "Quantity: "<< quantity << endl;
+void Product::display() {
+  cout << "ID: " << id << endl;
+  cout << "Name: " << name << endl;
+  cout << "Price: " << price << endl;
+  cout << "Quantity: " << quantity << endl;
 }
-
